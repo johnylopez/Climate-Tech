504dee9e183548466abace6353b5bbac108e0185
johnylopez <johnycrack21@hotmail.com>
2025-10-06 21:01:17 -0500

    Fixed path

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -20,7 +20,7 @@ const VideoBackground = ({ className }) => {
         className="absolute inset-0 w-full h-full object-cover"
       >
         <source
-          src="src/assets/video.mp4"
+          src="public/video.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
